497a9e1728be560bf1b48d72a6ded55382bb224e
ZhangHanbin <thebestzhq@gmail.com>
2026-03-07 20:26:17 +0800

    Initial Commit

---
+++ AIChat Watch App/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat Watch App
+//
+//  Created by zhb on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct AIChat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
